d1922e0edf7dc8b8965ecc1759856b48e3262641
Bukhtyar Haider <bukhtyar.haider1@gmail.com>
2024-01-05 01:22:42 +0500

    Setup Project

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+import "./main.scss";
+
+interface ThemeContextProps {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextProps | null>(null);
+
+function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="app-container" id={theme}>
+        <h2>Home</h2>
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/assets/index.ts
@@ -0,0 +1,25 @@
+export { default as headerSvg } from "./icons/header.svg";
+export { default as headerPng } from "./images/header.png";
+export { default as decentralizationControlSvg } from "./icons/decentralization-control.svg";
+export { default as decentralizationControlPng } from "./images/decentralization-control.png";
+export { default as joinChipherstarSvg } from "./icons/join-chipherstar.svg";
+export { default as joinChipherstarPng } from "./images/join-chipherstar.png";
+export { default as patternFooterSvg } from "./icons/pattern-footer.svg";
+export { default as patternFooterPng } from "./images/pattern-footer.png";
+export { default as securityFirstSvg } from "./icons/security-first.svg";
+export { default as securityFirstPng } from "./images/security-first.png";
+export { default as feature1Svg } from "./icons/feature-1.svg";
+export { default as feature2Svg } from "./icons/feature-2.svg";
+export { default as feature3Svg } from "./icons/feature-3.svg";
+export { default as feature4Svg } from "./icons/feature-4.svg";
+export { default as feature1Png } from "./images/feature-1.png";
+export { default as feature2Png } from "./images/feature-2.png";
+export { default as feature3Png } from "./images/feature-3.png";
+export { default as feature4Png } from "./images/feature-4.png";
+export { default as arrow } from "./icons/arrow.svg";
+export { default as youtube } from "./icons/youtube.svg";
+export { default as facebook } from "./icons/facebook.svg";
+export { default as instagram } from "./icons/instagram.svg";
+
+
+
