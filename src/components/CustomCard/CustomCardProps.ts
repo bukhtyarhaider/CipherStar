389a036f4b678d9febea4a26691de8057a2ef4f9
Bukhtyar Haider <93026518+bukhtyarhaider@users.noreply.github.com>
2024-01-18 01:52:57 +0500

    Added Custom Card Component (#21)

---
+++ src/components/CustomCard/CustomCardProps.ts
@@ -0,0 +1,16 @@
+/**
+ * Props for the CustomCard component.
+ */
+interface CustomCardProps {
+  /**
+   * The title of the Custom card.
+   */
+  title: string;
+
+  /**
+   * The description or content of the Custom card.
+   */
+  description: string;
+}
+
+export default CustomCardProps;
